ecab3213a114188a22141e6162aa1c6b3e7db7ad
mihirkelkar <kelkarmhr@gmail.comcd>
2014-07-14 22:24:28 -0700

    Half of tic tac toe game

--- location/location/ViewController.swift
+++ location/location/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import MapKit
+import corelocation
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, MKMapViewDelegate{
+    
+    
+    
+    @IBOutlet var mapView: MKMapView
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var userLocation:CLLocationCoordinate2D = mapView.userLocation.location.coordinate
+        
+        var latitudeDelta: CLLocationDegrees = 0.01
+        var longitudeDelta: CLLocationDegrees = 0.01
+        
+        var theSpan: MKCoordinateSpan = MKCoordinateSpanMake(latitudeDelta, longitudeDelta)
+        var theRegion: MKCoordinateRegion = MKCoordinateRegionMake(userLocation, theSpan)
+        self.mapView.setRegion(theRegion, animated: true)
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
